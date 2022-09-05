55b853e70ecd51827246045fd8ff0126750fcc0c
Zahid <zahidk.8850@gmail.com>
2022-09-05 15:29:32 +0530

    first-commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,22 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styled from "styled-components";
+import Layout from "../components/Reusable/Layout";
+import ReusableButton from "../components/Reusable/ReusableButton";
+import ReusableContainer from "../components/Reusable/ReusableContainer";
 
 const Home: NextPage = () => {
-  return <Wrapper>HEIHEIHEI</Wrapper>;
+  return (
+    <Layout>
+      <ReusableContainer>
+        <ReusableButton
+          goto="/dashboard"
+          title={"Click here to navigate to dashboard"}
+        />
+      </ReusableContainer>
+      ;
+    </Layout>
+  );
 };
 
 export default Home;
